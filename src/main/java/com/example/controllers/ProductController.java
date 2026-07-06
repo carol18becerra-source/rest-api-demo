73911ea94c3ec876d614d294aae3ca18c453620e
Carolina <carol18becerra@gmail.com>
2026-07-06 12:13:59 +0200

    antes de crear la rama sibida de imagen del producto

--- src/main/java/com/example/controllers/ProductController.java
+++ src/main/java/com/example/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +11,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,32 +24,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.entities.Product;
 import com.example.services.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 
 /**
  * La anotacion @RestController es para que todos los metodos que van a ser
- * creados dentro de este
- * controlador y reciben peticiones a través del protocolo HTTP, mediante los
- * verbos correspondientes
- * (GET, POST, PUT, DELETE, PATCH, etc.) devuelvan o reciban datos en formato de
- * JSON (JavaScript Object Notation)
+ * creados dentro de este controlador y reciben peticiones a través del
+ * protocolo HTTP, mediante los verbos correspondientes (GET, POST, PUT, DELETE,
+ * PATCH, etc.) devuelvan o reciban datos en formato de JSON (JavaScript Object
+ * Notation)
  */
-
 @RequestMapping("/products")
 /**
  * Una API REST esta orientada al recurso, es decir, que el controlador necesita
- * que se le especifique
- * que recurso va a responder, por ejemplo en esto seria /productos, y en
- * dependencia del verbo del protocolo
- * HTTP se estaria haciendo una peticion (request) contreta. Por ejemplo: Si el
- * verbo es GET, significa
+ * que se le especifique que recurso va a responder, por ejemplo en esto seria
+ * /productos, y en dependencia del verbo del protocolo HTTP se estaria haciendo
+ * una peticion (request) contreta. Por ejemplo: Si el verbo es GET, significa
  * que estamos solicitando todos los productos al recurso /productos. Si el
- * verbo es POST significa que queremos
- * recibir un producto en formato JSON, en el cuerpo de la peticion (request) y
- * persistirlo (guardarlo)
- * en las tablas correspondientes
+ * verbo es POST significa que queremos recibir un producto en formato JSON, en
+ * el cuerpo de la peticion (request) y persistirlo (guardarlo) en las tablas
+ * correspondientes
  */
 @RequiredArgsConstructor
 public class ProductController {
@@ -52,35 +53,30 @@ public class ProductController {
     private final ProductService productService;
 
     /**
-     * 
+     *
      * IMPORTANTE!!!
-     * 
+     *
      * Una API REST tiene que devolver informacion respecto a como ha sido
-     * solucionada la peticion (request),
-     * por ejemplo: el codigo 200 significa estado OK de la peticion, el codido 201
-     * significaria CREATED,
-     * el codigo 500 significaria que el servidor no ha podido cumplimentar la
-     * peticion, el codigo 401 NO ENCONTRADO,
-     * el codigo 403 prohibido, etc. Todos estos codigos se pueden encontrar en el
-     * sitio de W3Schools
-     * 
+     * solucionada la peticion (request), por ejemplo: el codigo 200 significa
+     * estado OK de la peticion, el codido 201 significaria CREATED, el codigo
+     * 500 significaria que el servidor no ha podido cumplimentar la peticion,
+     * el codigo 401 NO ENCONTRADO, el codigo 403 prohibido, etc. Todos estos
+     * codigos se pueden encontrar en el sitio de W3Schools
+     *
      * https://www.w3schools.com/tags/ref_httpmessages.asp
-     * 
+     *
      */
-
     /**
      * El metodo siguiente va a responder a una peticion (request) del tipo:
-     * 
+     *
      * http://localhost:8080/productos?page=0&size=3
-     * 
-     * Donde los parametros page y size seran utilizados para la paginacion, y no
-     * seran requeridos, es decir,
-     * que no son obligatorios que se suministren. Y en caso de NO ser suministrados
-     * (page y size),
-     * los productos se van a devolver ordenados.
-     * 
+     *
+     * Donde los parametros page y size seran utilizados para la paginacion, y
+     * no seran requeridos, es decir, que no son obligatorios que se
+     * suministren. Y en caso de NO ser suministrados (page y size), los
+     * productos se van a devolver ordenados.
+     *
      */
-
     @GetMapping
     public ResponseEntity<Map<String, Object>> dameProductos(
             @RequestParam(name = "page", required = false) Integer page,
@@ -110,15 +106,14 @@ public class ProductController {
 
     /**
      * El metodo siguente recupera un producto por el id que se recibe como una
-     * variable en la ruta mediante un end point (URl o URI) que tiene el formato
-     * siguiente
-     * 
+     * variable en la ruta mediante un end point (URl o URI) que tiene el
+     * formato siguiente
+     *
      * http://localhost:8080/productos/1
-     * 
+     *
      * donde el valor 1 al final del end point seria el id del producto
-     * 
+     *
      */
-
     @GetMapping("/{id}")
     @SuppressWarnings("Convert2Diamond")
     public ResponseEntity<Map<String, Object>> findProductById(
@@ -130,25 +125,70 @@ public class ProductController {
         try {
             Product product = productService.findById(product_id);
             if (product != null) {
-                String successMessage = "El producto con id" + product_id + "ha sido encontrado";
+                String successMessage = "El producto con id " + product_id + " ha sido encontrado";
 
                 responseAsMap.put("Mensaje todo OK: ", successMessage);
-                responseAsMap.put("producto encontrado", product);
-                responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.OK);
+                responseAsMap.put("producto encontrado ", product);
+                responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.OK);
             } else {
-                String failureMessage = "No ha sido encotrado ningun producto" + product_id;
+                String failureMessage = "No ha sido encotrado ningun producto con id: " + product_id;
                 responseAsMap.put("Error: ", failureMessage);
                 responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.NOT_FOUND);
 
-
             }
         } catch (DataAccessException e) {
-            String errorMessage = "Error grave al buscar el producto con id" + product_id + ", y la causa mas probable es: " + e.getMostSpecificCause().getMessage();
+            String errorMessage = "Error grave al buscar el producto con id" + product_id
+                    + ", y la causa mas probable es: " + e.getMostSpecificCause().getMessage();
             responseAsMap.put("Error grave: ", errorMessage);
             responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
 
         return responseEntity;
+    }
+
+    /**
+     * Metodo que recibe por POST el producto para ser persistido, guardado, y
+     * que valida el JSON recibido para comprobar si esta bien formado o no
+     */
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> saveProduct(
+            @Valid @RequestBody Product product,
+            BindingResult result) {
+        List<String> mensajesDeError = new ArrayList<>();
+
+        Map<String, Object> responseAsMap = new HashMap<>();
+
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+
+        // Primero comprobar si hay errorres en el producto recibido
+        if (result.hasErrors()) {
+            // Recuperamos los errores que tiene el producto recibido y se lo informamos al
+            // que realizó la petición (request) de persistir el producto
+            List<ObjectError> objectErrors = result.getAllErrors();
+
+            objectErrors.stream().forEach(objectError -> mensajesDeError.add(objectError.getDefaultMessage()));
+
+            responseAsMap.put("El producto tiene los siguientes errores: ", mensajesDeError);
+
+            responseAsMap.put("Producto mal formado", product);
+
+            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.BAD_REQUEST);
+
+            return responseEntity;
+        }
+
+        // Pesistimos el producto porque ya esta bien formado
+        try {
+            Product productoPersistido = productService.save(product);
+            responseAsMap.put("mensaje: ", "Producto persistido exitosamente");
+            responseAsMap.put("producto Persistido: ", productoPersistido);
+            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.CREATED);
+        } catch (DataAccessException e) {
+            responseAsMap.put("Error Grave", "No ha podido ser guardado el producto y la causa más probable es: "
+                    + e.getMostSpecificCause().getMessage());
+            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+            return responseEntity;
     }
 }
